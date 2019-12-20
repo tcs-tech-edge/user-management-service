f68eeaa91e1412502e606115bc5efa2ae091c1cf
murugeshkumark <master4uall@gmail.com>
2019-12-19 21:55:29 -0500

    Added base spring boot project

---
+++ src/main/java/com/techedge/spring/ums/UserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.techedge.spring.ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techedge/spring/ums/UserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techedge.spring.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
