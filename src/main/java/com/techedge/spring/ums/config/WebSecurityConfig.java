dfb8806566ed75b06cd66eda76d5b993893578cc
murugeshkumark <master4uall@gmail.com>
2019-12-20 02:04:19 -0500

    Minor clean up

--- src/main/java/com/techedge/spring/ums/config/WebSecurityConfig.java
+++ src/main/java/com/techedge/spring/ums/config/WebSecurityConfig.java
@@ -3,9 +3,7 @@ package com.techedge.spring.ums.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
