2d9c56603584f9a665f8f629db5908e6387e3d4b
murugeshkumark <master4uall@gmail.com>
2019-12-20 01:53:30 -0500

    Added WebSecurityConfig

---
+++ src/main/java/com/techedge/spring/ums/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.techedge.spring.ums.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance())
+                .withUser("user1")
+                .password("secret1")
+                .roles("USER")
+                .and()
+                .withUser("admin1")
+                .password("secret1")
+                .roles("USER", "ADMIN");
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .hasRole("USER").and()
+                .csrf()
+                .disable()
+                .headers()
+                .frameOptions()
+                .disable();
+    }
+}
