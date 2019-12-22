a60daa8fc44edd90ffc2767b6d6c45e86760b58a
murugeshkumark <master4uall@gmail.com>
2019-12-21 23:29:35 -0500

    Changed packing to was and extended SpringBootServletInitializer to deploy to tomcat

--- src/main/java/com/techedge/spring/ums/UserManagementServiceApplication.java
+++ src/main/java/com/techedge/spring/ums/UserManagementServiceApplication.java
@@ -2,9 +2,10 @@ package com.techedge.spring.ums;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserManagementServiceApplication {
+public class UserManagementServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementServiceApplication.class, args);
